a93409eb8403737ff3321784999f122b3690e321
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-12 15:44:23 +0100

    02-second es

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,6 +10,8 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
+console.log("N1---------------------------------------------------------------------------------------");
+
 console.log(`normal: ${teachers}`);
 const reversedTeachers = teachers.slice().reverse(); //slice to copy the original array and reversing the copy to avoid mutating original array
 console.log(`array still normal after reversing: ${teachers}`);
@@ -17,13 +19,28 @@ console.log(`reversed: ${reversedTeachers}`);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+console.log("N2---------------------------------------------------------------------------------------");
+const longNames = [];
+for (let i = 0; i < teachers.length; i++) {
+  console.log(`cycle ${i}, the name is ${teachers[i]}, lenght is ${teachers[i].length}`);
+
+  if (teachers[i].length >= 5) {
+    longNames.push(teachers[i]);
+    console.log(`the name ${teachers[i]} is longer than 5! Added.`);
+  }
+}
+console.log(`the names longer than 4 are: ${longNames}`);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+console.log("N3---------------------------------------------------------------------------------------");
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
+console.log("N4---------------------------------------------------------------------------------------");
+
 const isFabioPresent = null;
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
+console.log("N5---------------------------------------------------------------------------------------");
+
 const teachersString = null;
